2977c1ee071cea4f3387c83b25728d81bd93838b
mateuspelegrino <mateuspelegrino@gmail.com>
2014-10-05 22:32:57 -0300

    correção de erros e inclusão de hit rate

--- src/simuladormemoria/SimuladorMemoria.java
+++ src/simuladormemoria/SimuladorMemoria.java
@@ -9,7 +9,13 @@ public class SimuladorMemoria {
 
     public static void main(String[] args) {
         Scanner leituraDaTela = new Scanner(System.in);
-        int politica;
+        int politica, contador;
+        int acertos =0;
+        float fracaoAcertosErros =0;
+        int totalQuadros;
+        String dadoLido;
+        ArrayList<Pagina> memoria;
+        boolean paginaEstaNaMemoria;
         do {
             System.out.println("Qual tipo de política de substituição de quadros deseja usar?\n");
             System.out.println("Digite: 1 para lru;\n\t 2 para fifo;\n\t 3 para random.\n");
@@ -19,16 +25,17 @@ public class SimuladorMemoria {
         try {
             Pagina aux;
             BufferedReader in = new BufferedReader(new FileReader("entrada.txt"));
-            int totalQuadros = Integer.parseInt(args[0]);
-            String dadoLido = in.readLine();
-            ArrayList<Pagina> memoria = new ArrayList<>(totalQuadros);//memoria tem o tamanho passado como argumento
-            int contador = -1;  //indica posicao de memoria atual
-            boolean paginaEstaNaMemoria = false;
+            totalQuadros = Integer.parseInt(args[0]);
+             dadoLido = in.readLine();
+            memoria = new ArrayList<>(totalQuadros);//memoria tem o tamanho passado como argumento
+            contador = -1;  //indica posicao de memoria atual
+            paginaEstaNaMemoria = false;
             while (dadoLido != null) {
-                for (int i = 0; ((i <= contador + 1) && (i < totalQuadros)); i++) {
+                for (int i = 0; ((i < contador ) && (i < totalQuadros)); i++) {
                     if (memoria.get(i).getNumeroPagina() == Integer.parseInt(dadoLido)) {
                         memoria.get(i).setUltimaUtilizacao(++contador);
                         paginaEstaNaMemoria = true;
+                        acertos++;
                         break;
                     }
                 }
@@ -57,13 +64,21 @@ public class SimuladorMemoria {
                     }
                 }
                 paginaEstaNaMemoria = false;
+                imprimeMemoria(memoria,contador);
+                dadoLido = in.readLine();
             }
+            System.out.println("contador "+contador);
+            System.out.println("acertos "+acertos);
+            fracaoAcertosErros = (contador < 0)? 0 : (float)acertos/(float)(contador+1); //se contador <0 fração de acertos é 0
+            //caso contrário, a fração de acertos é acertos/contador+1, porque o contador começa em -1 e o primeiro quadro entra no tempo 0.
+
+            System.out.println("Fração de Acertos = "+fracaoAcertosErros);
         } catch (Exception ex) {
             ex.printStackTrace();
         }
     }
 
-    public void imprimeMemoria(ArrayList<Pagina> memoria, int contador) {
+    public static void imprimeMemoria(ArrayList<Pagina> memoria, int contador) {
         System.out.println("Estado atual da memória no tempo " + contador);
         for (int i = 0; i < memoria.size(); i++) {
             if ((contador < memoria.size()) && (i > contador)) {
@@ -99,7 +114,7 @@ public class SimuladorMemoria {
     }
 
     public static ArrayList<Pagina> random(ArrayList<Pagina> memoria, Pagina paginaEscalonada) {
-        Pagina retirada = memoria.remove((int) (Math.random() * 10));
+        Pagina retirada = memoria.remove((int) (Math.random() * memoria.size()));
         memoria.remove(retirada);
         memoria.add(paginaEscalonada);
         return memoria;
